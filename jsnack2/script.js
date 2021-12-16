9c5d749dd4065fd73cdca1f218b6d5e62af203f1
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-16 15:54:07 +0100

    jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,3 +2,20 @@
 // Fai inserire un numero, che chiameremo N, all’utente.
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
 // Ogni volta che ne crei uno, stampalo a schermo.
+
+const nArray = parseInt(prompt("Quanti array vuoi generare?"));
+const container = document.querySelector(".container");
+
+for (let i = 0; i < nArray; i++) {
+    const newArray = [];
+
+    for (let p = 0; p < 10; p++) {
+        const newValue = Math.floor(Math.random() * 100 + 1)
+        newArray.push(newValue);
+    }
+
+    console.log(newArray);
+    const newElement = document.createElement("div");
+    newElement.innerHTML = [...newArray];
+    container.append(newElement);
+}
